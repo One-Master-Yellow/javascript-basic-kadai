41fa5b07c951bf6e33193915093ce831a3099bf4
One-Master-Yellow <149803197+One-Master-Yellow@users.noreply.github.com>
2024-04-13 23:16:48 +0900

    Add files via upload

---
+++ kadai_021/event.js
@@ -0,0 +1,14 @@
+const btn = document.getElementById('btn');
+ 
+
+btn.addEventListener('click', ()  => {
+    setTimeout(()  =>  {
+        
+        alert('ボタンをクリックしました');
+    }, 2000);   
+    text = 'none';
+});   
+
+
+
+
